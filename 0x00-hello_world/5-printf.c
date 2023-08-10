30592b120310001ac5fc098d8c0c6d07d735a227
youssef elaasri <ussefelaasri@gmail.com>
2023-08-10 05:07:14 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-our main function
+ * Return: return
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
