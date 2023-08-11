9cd4d159b8b17df48bc3140c77213fe0ca11ed93
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 15:32:52 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main-main func
+  *Retutn: return
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+}
