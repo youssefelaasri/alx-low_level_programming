4a98e9acb39ca40d91ac27a737f8381ab84722cd
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 14:43:46 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main-main funcion
+  *Return: return 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
