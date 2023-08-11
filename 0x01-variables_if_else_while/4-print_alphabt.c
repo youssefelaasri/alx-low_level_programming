fdbf9c5aaa31151d946fa2023ca09996b76d9100
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 04:49:19 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main-main
+  *Return: return 0
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+}
