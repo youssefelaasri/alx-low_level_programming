6e36ef3c55d180052d604bf107901f1d6959efdd
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 04:21:55 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main-main func
+  *Return: return func
+  */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
