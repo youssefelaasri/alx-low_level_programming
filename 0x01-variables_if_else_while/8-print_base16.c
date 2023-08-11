f14ccf69efac7499afd7f7ed10e208bb2d073cf6
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 14:59:19 -0700

    c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main-mian fun
+  *Return: retu
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 'a'; i <= f; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
