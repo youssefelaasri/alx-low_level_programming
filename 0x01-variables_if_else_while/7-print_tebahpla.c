11ec19606fde8b724f6381c5566880462ff40bc0
youssef elaasri <ussefelaasri@gmail.com>
2023-08-11 14:53:20 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main-main
+  *Return: returns 0
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
