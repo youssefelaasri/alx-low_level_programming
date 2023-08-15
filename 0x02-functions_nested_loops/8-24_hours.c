2e2bfc0a2f433d66bfad6eb5ecc2eb06ad37b051
youssef elaasri <ussefelaasri@gmail.com>
2023-08-15 15:58:58 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
-			{	
+			{
 				for (d = 0; d <= 9; d++)
 				{
 					if (a == 2 && b ==  4 && c == 0 && d == 0)
