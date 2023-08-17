c56ae32e7cb0ae3645d767b2d7c19113a5af2f02
youssef elaasri <ussefelaasri@gmail.com>
2023-08-17 01:54:10 -0700

    c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_isupper - func
+  *Return: depends on
+  *@c: para
+  */
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *mul - func
+  *Return: retun multip
+  *@a: first number
+  *@b: second para
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
