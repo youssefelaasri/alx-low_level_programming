f77af7a55a9bace180d61718e39a06276b05ef65
youssef elaasri <ussefelaasri@gmail.com>
2023-08-20 02:05:24 -0700

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,10 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			printf("%2d", i * j);
+			if (j == 0)
+				printf("%d", i * j);
+			else
+				printf("%2d", i * j);
 			if (j < 9)
 			{
 				printf(", ");
