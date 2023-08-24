c379b4f9222b0537ce41e380a12beae80bc12f29
youssef elaasri <ussefelaasri@gmail.com>
2023-08-24 12:57:22 -0700

    v

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *string_toupper - fun
+  *@s: para
+  *Return: retu
+  */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
