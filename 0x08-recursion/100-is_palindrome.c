a5e37c20248114d04955e4280c8769c25389a078
youssef elaasri <ussefelaasri@gmail.com>
2023-08-31 02:00:59 -0700

    'alx_learning'

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,30 @@
 #include "main.h"
-
+/**
+  *is_pal - fin
+  *@s: string
+  *@start: first
+  *@end: last
+  *Return: dep
+  */
 int is_pal(char *s, int start, int end)
 {
 	if (s[start] == '\0')
-		return 1;
+		return (1);
 	if (s[start] != s[end])
-		return 0;
+		return (0);
 	else
-		return is_pal(s, start++, end--);
-
-
+		return (is_pal(s, start + 1, end - 1));
+}
+/**
+  *is_palindrome - fun
+  *@s: string
+  *Return: dep
+  */
 int is_palindrome(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
-	return is_pal(s, 0, i - 1);
+	return (is_pal(s, 0, i - 1));
 }
