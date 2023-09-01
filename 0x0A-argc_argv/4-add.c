973070a991c40bf87c653269432a3d37d479541e
youssef elaasri <ussefelaasri@gmail.com>
2023-09-01 04:14:31 -0700

    'alx_learning'

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  *main - fun
+  *@argc: coun
+  *@argv: arra
+  *Return: dep
+  */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+	char *p;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+
+	for (i = 1; i < argc; i++)
+	{
+		p = argv[i];
+		for (j = 0; p[j] != '\0'; j++)
+		{
+			if (!isdigit(p[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		if (atoi(argv[i]) >= 0)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
