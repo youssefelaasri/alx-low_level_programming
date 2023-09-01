8522b266d25b09862ef41af74016d6da386f23cb
youssef elaasri <ussefelaasri@gmail.com>
2023-09-01 02:39:30 -0700

    'alx_learning'

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - fun
+  *@argc: count
+  *@argv: array
+  *Return: ret 0
+  */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
