cbc1f4061ac3306b64c2808903388c7704167c6d
youssef elaasri <ussefelaasri@gmail.com>
2023-09-01 07:21:23 -0700

    'alx_learning'

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,6 +1,35 @@
 #include "main.h"
+/**
+  *_strlen - fun
+  *@s: para
+  *Return: return
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+	char c;
 
+	for (c = s[0]; c != '\0'; c = *(s + i))
+		i++;
+	return (i);
+}
+/**
+  *_strncat - fun
+  *@dest: para 1
+  *@src: para 2
+  *@n: para 3
+  *Return: ret
+  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int id = _strlen(dest);
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		dest[id + i] = src[i];
+		if (src[i] == '\0')
+			break;
+	}
+	return (dest);
 }
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-/**
-  *_atoi - fun
-  *@s: para
-  *Return: 0 alw
-  */
 int _atoi(char *s)
 {
 	return (0);
