619afc43b8fbecec0d19eaed725279aed94462e5
youssef elaasri <ussefelaasri@gmail.com>
2023-09-04 20:30:31 +0100

    d

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*alloc_grid - fun
+*@width: ei
+*@height: heth
+*Return: dep
+*/
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(height * sizeof(int));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			p[i] = malloc(width * sizeof(int));
+			for (j = 0; j < width; j++)
+			{
+				p[i][j] = 0;
+			}
+		}
+	}
+	return (p);
+}
