9281b095a01e3e9bc6290fdf41a3befd3831dbbb
youssef elaasri <ussefelaasri@gmail.com>
2023-09-17 04:54:30 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog {
+#ifndef dog_H
+#define dog_H
+/**
+  *struct dog - our struct
+  *@name: nmae
+  *@age: our age
+  *@owner: owner
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
