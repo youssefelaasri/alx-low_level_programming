5bd2ef4cbc58767a1e7e5e88583905da85f1ba18
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 20:03:18 +0100

    d

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
-*_strlen - fun
-*@str: string
-*Return: ret
-*/
-size_t _strlen(const char *str)
-{
-	size_t length = 0;
 
-	while (*str != '\0')
-	{
-		length++;
-		str++;
-	}
-
-	return (length);
-}
 
 /**
 *print_list - fun
@@ -27,14 +11,7 @@ size_t _strlen(const char *str)
 size_t print_list(const list_t *h)
 {
 	int c = 0;
-	int j = 0;
 	const list_t *ptr = NULL;
-
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
 
 	ptr = h;
 	while (ptr != NULL)
@@ -46,8 +23,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			j = _strlen(ptr->str);
-			printf("[%d] %s\n", j, ptr->str);
+			printf("[%d] %s\n", h->len, ptr->str);
 		}
 		ptr = ptr->next;
 	}
