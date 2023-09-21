4bc1062cfba501ed5712e894b866a003c5193d99
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 21:33:08 +0100

    d

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*add_node - fun
+*@head: header
+*@str: strin
+*Return: dep
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *pr = malloc(sizeof(list_t));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (pr == NULL)
+	{
+		return (NULL);
+	}
+	pr->str = strdup(str);
+	if (pr->str == NULL)
+	{
+		return (NULL);
+	}
+	pr->next = *head;
+	*head = pr;
+	return (pr);
+}
