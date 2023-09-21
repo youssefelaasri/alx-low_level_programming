62e141d5e7e6d59a7f122fe0436b797d8aa1f08b
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 20:12:14 +0100

    d

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
-
 
 /**
 *print_list - fun
-*@h: struct
+*@h: head
 *Return: dep
 */
 size_t print_list(const list_t *h)
 {
 	size_t c = 0;
-	const list_t *ptr = NULL;
 
-	ptr = h;
-	while (ptr != NULL)
+	while (h != NULL)
 	{
-		if (ptr->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, ptr->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
+		h = h->next;
 		c++;
-		ptr = ptr->next;
 	}
 	return (c);
 }
