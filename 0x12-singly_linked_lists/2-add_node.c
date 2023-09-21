1eb1ab8a8e94a9c95dab0d1b355ae5753f62e769
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 15:54:09 -0700

    s

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+  *_strlen - fun
+  *@s: dtri
+  *Return: ret
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
 
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
+}
 /**
 *add_node - fun
 *@head: header
@@ -13,12 +29,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *pr = malloc(sizeof(list_t));
+	int len = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
+	len = _strlen(str);
 	if (pr == NULL)
 	{
 		return (NULL);
@@ -28,6 +45,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	pr->len = len;
 	pr->next = *head;
 	*head = pr;
 	return (pr);
