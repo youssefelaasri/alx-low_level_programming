a53540f73a38df9830a3c05392f5e178505a37ea
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 05:33:38 -0700

    d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog;
+	dog_t *new_Dog = malloc(sizeof(dog_t));
+	if (new_Dog == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_Dog->name, name);
+	new_Dog->age = age;
+	strcpy(new_Dog->owner, owner);
+	return(new_Dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
