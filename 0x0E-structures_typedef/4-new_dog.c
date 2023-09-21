9e556ec27c664832b6805b3563513f849b34ba1c
youssef elaasri <ussefelaasri@gmail.com>
2023-09-21 05:42:36 -0700

    d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include "dog.h"
 #include <string.h>
-
+/**
+  *new_dog - fun
+  *@name: names
+  *@age: ages
+  *@owner: owners
+  *Return: ret
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_Dog = malloc(sizeof(dog_t));
+
 	if (new_Dog == NULL)
 	{
 		return (NULL);
 	}
 	new_Dog->name = strdup(name);
 	new_Dog->age = age;
-	new_Dog->owner = strdup(owner);;
-	return(new_Dog);
+	new_Dog->owner = strdup(owner);
+	return (new_Dog);
 }
