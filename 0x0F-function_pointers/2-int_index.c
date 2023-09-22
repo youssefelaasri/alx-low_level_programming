05a863a57a9b2ccf82375cc051db18c07b7d79e1
youssef elaasri <ussefelaasri@gmail.com>
2023-09-22 23:11:35 +0100

    f

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+*int_index - fun
+*@array: ourb array
+*@size: size of the
+*@cmp: our func
+*Return: depends
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
