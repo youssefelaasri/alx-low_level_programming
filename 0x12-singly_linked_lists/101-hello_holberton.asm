7497b801645d2f4694cb0d846f50207765736fa9
youssef elaasri <ussefelaasri@gmail.com>
2023-09-22 02:14:22 +0100

    g

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,8 @@
-    org  0x100        ; .com files always start 256 bytes into the segment
+org  0x100
 
-    ; int 21h is going to want...
-
-    mov  dx, msg      ; the address of or message in dx
-    mov  ah, 9        ; ah=9 - "print string" sub-function
-    int  0x21         ; call dos services
-
-    mov  ah, 0x4c     ; "terminate program" sub-function
-    int  0x21         ; call dos services
-
-    msg  db 'Hello, Holberton', 0x0d, 0x0a, '$'   ; $-terminated message
+mov  dx, msg
+mov  ah, 9
+int  0x21
+mov  ah, 0x4c
+int  0x21
+msg  db 'Hello, Holberton', 0x0d, 0x0a, '$'
