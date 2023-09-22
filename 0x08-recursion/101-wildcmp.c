5741c4a95e9949ce5515477fa7835964e22c8431
youssef elaasri <ussefelaasri@gmail.com>
2023-09-22 23:54:16 +0100

    h

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,23 @@
-#include "main.h"
-
-
+/**
+*wildcmp - fun
+*@s1: first
+*@s2: second
+*Return: depen
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
+	}
+	if (*s1 == *s2 || *s2 == '?')
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
