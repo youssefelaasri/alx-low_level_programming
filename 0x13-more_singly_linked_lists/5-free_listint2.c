2ab62b1cd7ccfa9495720c39ebd062134ed5b4b4
youssef elaasri <ussefelaasri@gmail.com>
2023-09-26 09:20:11 +0100

    g

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_listint2 - fun
+*@head: hed
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		free(*head);
+		*head = ptr;
+	}
+	*head = NULL;
+}
