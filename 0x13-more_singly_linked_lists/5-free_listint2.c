caafe40e5d2610084dbb5ce5bb2cb4725ffb9a58
youssef elaasri <ussefelaasri@gmail.com>
2023-09-26 10:08:53 +0100

    f

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,9 +6,13 @@
 *free_listint2 - fun
 *@head: hed
 */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
+
+	if (head == NULL || *head == NULL)
+		return;
 
 	ptr = *head;
 	while (ptr != NULL)
