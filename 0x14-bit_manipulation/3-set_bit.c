5db298af99e89c3717f705690d83b223abb93a3a
youssef elaasri <ussefelaasri@gmail.com>
2023-09-28 21:39:09 +0100

    f

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
-	if (index >= sizeof(unsigned long int))
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	*n = *n | (1 << index);
