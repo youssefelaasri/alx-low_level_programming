54f12b59cd708d674eee53aade6a75a64e3173e0
youssef elaasri <ussefelaasri@gmail.com>
2023-09-28 13:02:41 +0100

    g

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <math.h>
+/**
+*binary_to_uint - fun
+*@b: string
+*Return: dep
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1' || b[i] == '0')
+		{
+			result = result + (b[i] - 48) * pow(2, (strlen(b) - 1) - i);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef main_H
+#define main_H
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
