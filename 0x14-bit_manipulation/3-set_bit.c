5464a16b52d72174d3ab646fb57c1b20f6cbbec2
youssef elaasri <ussefelaasri@gmail.com>
2023-09-28 21:37:10 +0100

    f

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+/**
+*set_bit - fun
+*@n: num
+*@index: ind
+*Return: depends
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= sizeof(unsigned long int))
+		return (-1);
+
+	*n = *n | (1 << index);
+	return (1);
+
+}
