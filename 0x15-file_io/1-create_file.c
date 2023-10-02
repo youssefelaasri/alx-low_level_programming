cf4fe6d26c2842bb002e4f8e2406b393227dd464
youssef elaasri <ussefelaasri@gmail.com>
2023-10-02 20:36:31 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
+		{
+			
+		}
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
