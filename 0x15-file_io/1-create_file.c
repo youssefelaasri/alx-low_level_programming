2fb86569b71b90fa516365c3f066114b6659bad1
youssef elaasri <ussefelaasri@gmail.com>
2023-10-02 20:35:23 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,26 +7,26 @@
 *@text_content: text content
 *Return: depends
 */
+
 int create_file(const char *filename, char *text_content)
 {
-	int o;
-	ssize_t w, l;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l] != '\0'; l++)
-		{
-
-		}
+		for (len = 0; text_content[len]; len++)
 	}
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, l);
-	if (w == -1 || o == -1)
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
+
 	return (1);
 }
