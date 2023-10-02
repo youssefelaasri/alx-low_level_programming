e9b1d841499ec5688313f92b0b8b7ac0b03e8155
youssef elaasri <ussefelaasri@gmail.com>
2023-10-02 20:28:56 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,5 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
 *create_file - fun
@@ -14,7 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	l = strlen(text_content);
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l] != '\0'; l++)
+		{
+
+		}
+	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, l);
 	if (w == -1 || o == -1)
