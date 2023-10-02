5986e032c443cc25ca89ed087d57372c9066a085
youssef elaasri <ussefelaasri@gmail.com>
2023-10-02 18:08:24 +0100

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,32 +17,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(fd);
 		return (0);
 	}
 
+	fd = open (filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-	n = read(fd, buffer, letters);
-	if (n < 0)
+
+
+
+	n = read (fd, buffer, letters);
+	if (n == -1)
 	{
 		free(buffer);
-		close(fd);
+		close (fd);
 		return (0);
 	}
 
 	m = write(STDOUT_FILENO, buffer, n);
-	if (m < 0 || m != n)
+	if (m == -1 || m != n)
 	{
 		free(buffer);
-		close(fd);
+		close (fd);
 		return (0);
 	}
+	free (buffer);
+	close (fd);
 	return (m);
 }
