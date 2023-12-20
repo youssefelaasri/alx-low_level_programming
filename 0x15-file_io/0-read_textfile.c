cbdb3068dcd2a5b0156836f3eac91940cfe01dae
youssef elaasri <ussefelaasri@gmail.com>
2023-12-20 10:38:53 -0800

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+*read_textfile - fun
+*@filename: file
+*@letters: number of letters
+*Return: depends
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n, m;
+	ssize_t fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+
+
+
+	n = read(fd, buffer, letters);
+	if (n == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	m = write(STDOUT_FILENO, buffer, n);
+	if (m == -1 || m != n)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (m);
+}
